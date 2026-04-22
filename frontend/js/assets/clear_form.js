7bd9307194d198ad514f2a971aecd5180576e947
renato <renatoviturino80@gmail.com>
2026-04-22 12:24:52 -0300

    Atualização na limpeza do formulário pós cadastro.

--- frontend/js/assets/clear_form.js
+++ frontend/js/assets/clear_form.js
@@ -1,7 +1,7 @@
-export function clean_form(formInputs){
+export function clean_form(formInputs) {
     // document.querySelectorAll("form input");
 
-    for(let input of formInputs) {
-        input.value = "";
+    for (let input of formInputs) {
+        input.id === "breed" || input.id === "gender" ? input.options[0].setAttribute("selected", "true") : input.value = "";
     }
 }
